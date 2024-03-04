866b9c7917532d0132abcfbfd295f7ec73f68432
david <hoong.way2018@gmail.com>
2024-03-04 12:07:09 -0500

    update

--- Form1.cs
+++ Form1.cs
@@ -71,7 +71,7 @@ namespace TimerAndAlerm
         {
             var beijintime = GetBeiJinTime();
             label5.Text= beijintime.ToString("HH:mm:ss");
-            if (beijintime.Hour == 0 || beijintime.Hour == 5 || beijintime.Hour == 11 || beijintime.Hour == 17 || beijintime.Hour == 23)
+            if (beijintime.Hour == 5 || beijintime.Hour == 11 || beijintime.Hour == 17 || beijintime.Hour == 23)
             {
                 if (beijintime.Minute == 54 && beijintime.Second == 0)
                 {
